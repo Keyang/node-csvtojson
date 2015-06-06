54756db38fda4a022c1a7df237b2133679c97686
Tommy Dodson <t3.dodson@gmail.com>
2015-06-05 18:54:31 -0700

    Added compileDependencies for wiki automation

--- devops/runMadge.js
+++ devops/runMadge.js
@@ -1,6 +1,6 @@
 var exec = require('child_process').exec;
 function madge () {
-	exec('madge -x "node_modules|util|stream|os|assert|fs|http|child_process" -i graph.png ./', function (err, stdout, stderr) {
+	exec('madge -x "node_modules|util|stream|os|assert|fs|http|child_process" -i graph.png ./', function (err) {
 		if (err) {
 			console.log('could not run madge: ' + err);
 		}
